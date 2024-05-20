3387e418328eddad043a943199a467ebfab51fb1
Vadym Hutei <vhutei@alliancedigital.tech>
2024-05-20 15:22:30 +0300

    правки

--- main.py
+++ main.py
@@ -14,11 +14,26 @@ db = client[os.environ['MONGO_DBNAME']]
 collection_from = db[os.environ['MONGO_COLLECTION_FROM']]
 collection_to = db[os.environ['MONGO_COLLECTION_TO']]
 prop = os.environ['MONGO_PROP_TO_TRANSFER']
+chunk_size = int(os.environ['CHUNK_SIZE'])
 
-for doc in collection_from.find({}):
-    collection_to.update_one(
-        {'_id': doc['_id']},
-        {'$set': {prop: doc[prop]}},
-    )
+def process_chunk(chunk):
+    for doc in chunk:
+        collection_to.update_one(
+            {'_id': doc['_id']},
+            {'$set': {prop: doc[prop]}},
+        )
+
+cursor = collection_from.find()
+
+chunk = []
+
+for doc in cursor:
+    chunk.append(doc)
+    if len(chunk) >= chunk_size:
+        process_chunk(chunk)
+        chunk = []
+
+if chunk:
+    process_chunk(chunk)
 
 client.close()
